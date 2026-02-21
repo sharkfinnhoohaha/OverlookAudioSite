c3301113edb8f0365e901127c12061e4f0e62b40
sharkfinnhoohaha <163480942+sharkfinnhoohaha@users.noreply.github.com>
2026-02-21 05:03:18 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  // This tells Vite: "It is okay to show NEXT_PUBLIC variables to the browser"
+  envPrefix: ['VITE_', 'NEXT_PUBLIC_'], 
   define: {
-    // This allows Tina to access your keys without crashing the browser
-    'process.env.NEXT_PUBLIC_TINA_CLIENT_ID': JSON.stringify(process.env.NEXT_PUBLIC_TINA_CLIENT_ID),
-    'process.env.TINA_TOKEN': JSON.stringify(process.env.TINA_TOKEN),
-    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    'process.env': process.env
   }
 })
